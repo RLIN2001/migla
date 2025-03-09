86a0fe7368d69dd1aba31d4010a568549742767a
RLIN2001 <chenweiriccardo.lin@gmail.com>
2025-03-09 01:24:04 +0100

    chore(app): add general app flow

--- migla/Views/LoginView.swift
+++ migla/Views/LoginView.swift
@@ -13,81 +13,92 @@ struct LoginView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                HStack {
-                    ZStack {
-                        Circle()
-                            .fill(Color.blue.opacity(0.1))
-                            .frame(width: 200, height: 300)
-                            .offset(x: -100, y: -50)
-                        
-                        Circle()
-                            .fill(Color.blue.opacity(0.1))
-                            .frame(width: 300, height: 200)
-                            .offset(x: -50, y: -100)
+        
+        NavigationStack {
+            
+            ZStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue.opacity(0.1))
+                                .frame(width: 200, height: 300)
+                                .offset(x: -100, y: -50)
+                            
+                            Circle()
+                                .fill(Color.blue.opacity(0.1))
+                                .frame(width: 300, height: 200)
+                                .offset(x: -50, y: -100)
+                        }
+                        Spacer()
                     }
                     Spacer()
                 }
-                Spacer()
-            }
-            
-            VStack(spacing: 25) {
-                Text("Welcome back")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top, 50)
                 
-                Image("welcome_image") 
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .padding(.vertical, 20)
-                
-                VStack(spacing: 20) {
-                    TextField("Email", text: $email)
-                        .textFieldStyle(CustomTextFieldStyle())
-                        .autocapitalization(.none)
+                VStack(spacing: 25) {
+                    Text("Welcome back")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.top, 50)
                     
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(CustomTextFieldStyle())
-                }
-                .padding(.horizontal, 20)
-                
-                Button(action: {
-                }) {
-                    Text("Forget password ?")
-                        .foregroundColor(.blue)
-                }
-                
-                Button(action: {
-                }) {
-                    Text("Login")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue.opacity(0.7))
-                        .cornerRadius(10)
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-                
-                HStack {
-                    Text("Don't have an account?")
+                    Image("welcome_image")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .padding(.vertical, 20)
+                    
+                    VStack(spacing: 20) {
+                        TextField("Email", text: $email)
+                            .textFieldStyle(CustomTextFieldStyle())
+                            .autocapitalization(.none)
+                        
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(CustomTextFieldStyle())
+                    }
+                    .padding(.horizontal, 20)
+                    
                     Button(action: {
-                        dismiss()
                     }) {
-                        Text("Sign Up")
+                        Text("Forget password ?")
                             .foregroundColor(.blue)
                     }
+                    
+                    Button(action: {
+                    }) {
+                        NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true)) {
+                            Text("Login")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue.opacity(0.7))
+                                .cornerRadius(10)
+                        }
+                     
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                    
+                    HStack {
+                        Text("Don't have an account?")
+                        Button(action: {
+                            dismiss()
+                        }) {
+                            NavigationLink(destination: RegistrationView().navigationBarBackButtonHidden(true)) {
+                                Text("Sign Up")
+                                    .foregroundColor(.blue)
+                            }
+                           
+                        }
+                    }
+                    .padding(.top, 10)
                 }
-                .padding(.top, 10)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemYellow).opacity(0.1))
+            .ignoresSafeArea()
+            .navigationBarBackButtonHidden(true)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(UIColor.systemYellow).opacity(0.1))
-        .ignoresSafeArea()
     }
 }
 
--- migla/Views/RegistrationView.swift
+++ migla/Views/RegistrationView.swift
@@ -14,85 +14,91 @@ struct RegistrationView: View {
     @State private var confirmPassword: String = ""
     
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                HStack {
-                    ZStack {
-                        Circle()
-                            .fill(Color.blue.opacity(0.1))
-                            .frame(width: 200, height: 300)
-                            .offset(x: -100, y: -50)
-                        
-                        Circle()
-                            .fill(Color.blue.opacity(0.1))
-                            .frame(width: 300, height: 200)
-                            .offset(x: -50, y: -100)
+        
+        NavigationStack {
+            ZStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue.opacity(0.1))
+                                .frame(width: 200, height: 300)
+                                .offset(x: -100, y: -50)
+                            
+                            Circle()
+                                .fill(Color.blue.opacity(0.1))
+                                .frame(width: 300, height: 200)
+                                .offset(x: -50, y: -100)
+                        }
+                        Spacer()
                     }
                     Spacer()
                 }
-                Spacer()
+                
+                VStack(spacing: 25) {
+                    Spacer()
+                        .frame(height: 60)
+                    
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                    
+                    Text("Welcome to MIGLA!")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text("Let's be together in your child's progress")
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.bold)
+                    
+                    
+                    VStack(spacing: 15) {
+                        TextField("Nome e cognome genitore", text: $fullName)
+                            .textFieldStyle(RoundedTextFieldStyle())
+                        
+                        TextField("Email", text: $email)
+                            .textFieldStyle(RoundedTextFieldStyle())
+                            .autocapitalization(.none)
+                        
+                        SecureField("Password", text: $password)
+                            .textFieldStyle(RoundedTextFieldStyle())
+                        
+                        SecureField("Conferma password", text: $confirmPassword)
+                            .textFieldStyle(RoundedTextFieldStyle())
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                    }) {
+                        Text("Sign Up")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue.opacity(0.7))
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    HStack {
+                        Text("Already have an account?")
+    
+                        NavigationLink(destination: LoginView().navigationBarBackButtonHidden(true)) {
+                            Text("Sign In")
+                                .foregroundColor(.blue)
+                        }
+                 
+                    }
+                    .padding(.vertical)
+                }
             }
-            
-            VStack(spacing: 25) {
-                Spacer()
-                    .frame(height: 60)
-                
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 100)
-                
-                Text("Welcome to MIGLA!")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                
-                Text("Let's be together in your child's progress")
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.bold)
-
-                
-                VStack(spacing: 15) {
-                    TextField("Nome e cognome genitore", text: $fullName)
-                        .textFieldStyle(RoundedTextFieldStyle())
-                    
-                    TextField("Email", text: $email)
-                        .textFieldStyle(RoundedTextFieldStyle())
-                        .autocapitalization(.none)
-                    
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(RoundedTextFieldStyle())
-                    
-                    SecureField("Conferma password", text: $confirmPassword)
-                        .textFieldStyle(RoundedTextFieldStyle())
-                }
-                .padding(.horizontal, 20)
-                
-                Spacer()
-                
-                Button(action: {
-                }) {
-                    Text("Sign Up")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue.opacity(0.7))
-                        .cornerRadius(10)
-                }
-                .padding(.horizontal, 20)
-                
-                HStack {
-                    Text("Already have an account?")
-                    Text("Sign In")
-                        .foregroundColor(.blue)
-                }
-                .padding(.vertical)
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(UIColor.systemYellow).opacity(0.1))
-        .ignoresSafeArea()
-    }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemYellow).opacity(0.1))
+            .ignoresSafeArea()
+        } }
 }
 
 struct RoundedTextFieldStyle: TextFieldStyle {
